360d9e16e7225b799768595f2ae85d5bf58866da
gasabe <abelardogaston@gmail.com>
2025-08-06 13:17:21 -0300

    changes

--- src/pages/Home/pages/Projects.jsx
+++ src/pages/Home/pages/Projects.jsx
@@ -2,16 +2,19 @@ import React from "react";
 import "../styles/Home.css";
 import ProjectCard from "../../Components/Card";
 import ubertoPic from "../../../../public/uberto.png";
+import merendappPic from "../../../../public/merendapp.jpg";
+import readappPic from "../../../../public/readapp.png";
+import tallerPic from "../../../../public/taller.jpg";
 
 export default function Projects() {
   return (
     <section className="home-container">
-        <h2 className="home-title">Proyectos</h2>
+      <h2 className="home-title">Proyectos</h2>
       <div className="home-card-container">
         <ProjectCard
           title="Gestor de Libros"
           description="App en React + Spring Boot para gestionar recomendaciones de libros"
-          image="https://raw.githubusercontent.com/algo3-unsam/readapp-2024-grupo-7/refs/heads/main/public/assets/loginFondo.png?token=GHSAT0AAAAAADHE3RTHBOWW2FXTLOGGS6QS2ETNPQQ"
+          image={readappPic}
           repoLink="https://github.com/gasabe/React-ReadApp.git"
         />
         <ProjectCard
@@ -23,8 +26,14 @@ export default function Projects() {
         <ProjectCard
           title="MerenApp"
           description="App en React + Spring Boot para gestionar donaciones a comedores"
-          image="https://avatars.githubusercontent.com/u/205374845?"
+          image={merendappPic}
           repoLink="https://github.com/Unsam-LosPerris/MerendApp-Front.git"
+        />
+        <ProjectCard
+          title="Taller"
+          description="App en React para gestionar un taller mecánico"
+          image={tallerPic}
+          repoLink="https://taller-mecanico-the-friends.netlify.app/"
         />
       </div>
     </section>
