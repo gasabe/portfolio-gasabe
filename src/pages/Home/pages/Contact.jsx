a26c84747986f6642cfe250cf17acbd25fa9d7c1
gasabe <abelardogaston@gmail.com>
2025-08-13 10:39:47 -0300

    fix

--- src/pages/Home/pages/Contact.jsx
+++ src/pages/Home/pages/Contact.jsx
@@ -8,38 +8,56 @@ export default function Contact() {
 
         <div className="contact-item">
           <img src="../whatsapp.png" alt="WhatsApp" className="contact-icon" />
-          <a href="https://wa.me/5491140654620" target="_blank" rel="noreferrer">
-            +54 9 11 4065 4620
-          </a>
+          <div className="list">
+            <a
+              href="https://wa.me/5491140654620"
+              target="_blank"
+              rel="noreferrer"
+            >
+              +54 9 11 4065 4620
+            </a>
+          </div>
         </div>
 
         <div className="contact-item">
           <img src="../gmail.png" alt="Email" className="contact-icon" />
-          <a href="mailto:abelardogaston@gmail.com">
-            abelardogaston@gmail.com
-          </a>
+          <div className="list">
+            <a href="mailto:abelardogaston@gmail.com">
+              abelardogaston@gmail.com
+            </a>
+            <a href="mailto:abelardosgaston@gmail.com">
+              abelardosgaston@gmail.com
+            </a>
+            <a href="mailto:gabelardo@estudiantes.unsam.edu.ar">
+              gabelardo@estudiantes.unsam.edu.ar
+            </a>
+          </div>
         </div>
 
         <div className="contact-item">
           <img src="../linkedin.png" alt="LinkedIn" className="contact-icon" />
-          <a
-            href="https://www.linkedin.com/in/gaston-ezequiel-abelardo-680b12187"
-            target="_blank"
-            rel="noreferrer"
-          >
-            LinkedIn
-          </a>
+          <div className="list">
+            <a
+              href="https://www.linkedin.com/in/gaston-ezequiel-abelardo-680b12187"
+              target="_blank"
+              rel="noreferrer"
+            >
+              LinkedIn
+            </a>
+          </div>
         </div>
 
         <div className="contact-item">
           <img src="../github.png" alt="GitHub" className="contact-icon" />
-          <a
-            href="https://github.com/gasabe"
-            target="_blank"
-            rel="noreferrer"
-          >
-            github.com/gasabe
-          </a>
+          <div className="list">
+            <a
+              href="https://github.com/gasabe"
+              target="_blank"
+              rel="noreferrer"
+            >
+              github.com/gasabe
+            </a>
+          </div>
         </div>
       </div>
     </section>
