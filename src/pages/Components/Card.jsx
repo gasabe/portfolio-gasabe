f20c31440bf1aab2dc05975932823c58fa36293d
gasabe <abelardogaston@gmail.com>
2025-08-13 00:30:29 -0300

    cambios generales

--- src/pages/Components/Card.jsx
+++ src/pages/Components/Card.jsx
@@ -3,7 +3,7 @@ import '../../../src/pages/Home/styles/ProjectCard.css';
 
 export default function ProjectCard({ title, description, image, repoLink, onClick }) {
   return (
-    <div className="project-card" onClick={onClick}>
+    <div className="project-card" >
       <img src={image} alt={title} className="project-image" />
       <div className="project-content">
         <h3 className="project-title">{title}</h3>
@@ -13,7 +13,7 @@ export default function ProjectCard({ title, description, image, repoLink, onCli
           target="_blank"
           rel="noopener noreferrer"
           className="project-btn"
-          onClick={(e) => e.stopPropagation()} // Evita que el click al botón cierre el modal
+          onClick={onClick} 
         >
           CONOCÉ MÁS
         </a>
